e854fa7aa413b1527f8936fe2f42743e7451329b
rohan.rawal <rohansinghrawal@gmail.com>
2022-03-08 15:59:33 +0530

    Detects food collision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up,"w")
@@ -22,6 +24,10 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
     
     
 
--- snake.py
+++ snake.py
@@ -7,6 +7,7 @@ class Snake:
     def __init__(self) -> None:
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -20,20 +21,20 @@ class Snake:
         for seg_num in range(len(self.segments)-1, 0, -1):
             pos = self.segments[seg_num-1].pos()
             self.segments[seg_num].goto(pos)
-        self.segments[0].forward(MOVING_DISTANCE)
+        self.head.forward(MOVING_DISTANCE)
 
     def up(self):
-        if self.segments[0].heading() != 270:
-            self.segments[0].setheading(90)
+        if self.head.heading() != 270:
+            self.head.setheading(90)
 
     def down(self):
-        if self.segments[0].heading() != 90:
-            self.segments[0].setheading(270)
+        if self.head.heading() != 90:
+            self.head.setheading(270)
 
     def left(self):
-        if self.segments[0].heading() != 0:
-            self.segments[0].setheading(180)
+        if self.head.heading() != 0:
+            self.head.setheading(180)
 
     def right(self):
-        if self.segments[0].heading() != 180:
-            self.segments[0].setheading(0)
+        if self.head.heading() != 180:
+            self.head.setheading(0)
